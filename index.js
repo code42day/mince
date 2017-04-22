c4d1bd68216f1b41045c2e807f64dfe583130026
Damian Krzeminski <pirxpilot@code42day.com>
2017-04-22 16:27:06 -0700

    add basic support for source maps
    --source-maps - enables source maps
    --source-root - allows for changing default source root - mincer
    generates '/' as source root

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var argv = require('optimist')
   .usage('Mince files and place them in destination directory.\nUsage: $0 [options] file..')
   .describe('destination', 'Location of minced files')
   .describe('include', 'Include path for mincer - can be specified multiple times.')
+  .describe('source-map', 'Name of the generated source map.')
+  .describe('source-root', 'Common path prefix for files include in source map.')
   .alias('d', 'destination')
   .alias('i', 'include')
   .demand('destination')
@@ -12,7 +14,6 @@ var argv = require('optimist')
 
 var mincer = require('mincer');
 var DebugComments = require('./lib/debug-comments.js');
-
 
 mincer.registerPostProcessor('application/javascript', DebugComments);
 
@@ -23,6 +24,12 @@ function toArray(a) {
 function environment(include) {
   include = toArray(include);
   var environment = new mincer.Environment(process.cwd());
+  if (argv['source-map']) {
+    environment.enable('source_map');
+  }
+  if ('source-root' in argv) {
+    environment.sourceRoot = argv['source-root'];
+  }
   include.forEach(function(i) {
     environment.appendPath(i);
   });
@@ -35,7 +42,7 @@ function writeFile(path, data) {
     // remove processing directives
     if (!line.startsWith('//(=)')) {
       output.write(line);
-      output.write('\n')
+      output.write('\n');
     }
   });
   output.end();
@@ -48,7 +55,13 @@ function mince(environment, src, destination) {
     if (!asset) {
       throw 'Cannot find: ' + s;
     }
-    writeFile(path.join(destination, s), asset.toString());
+    var outName = path.join(destination, s);
+    var sourceMappingComment = '';
+    if (argv['source-map'] && asset.sourceMap) {
+      sourceMappingComment = '\n//# sourceMappingURL=' + s + '.map';
+      fs.writeFile(outName + '.map', asset.sourceMap);
+    }
+    writeFile(outName, asset.toString() + sourceMappingComment);
   });
 }
 
